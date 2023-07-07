35fcc553c2d2f2cfd3175d6f17fd77f25669d277
kyle <davek2647@gmail.com>
2023-07-07 13:34:12 +0300

    new attempt

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const { expect } = require("chai");
-
-
 function scuberGreetingForFeet(feet){
-  prompt("input feet!!")
-  if (feet<=400)return "This one is on me!";
-  else if (feet>2000 && feet <=2500) return "I will gladly take your thirty bucks."
- else if (feet >2500) return "No can do."
- 
+  if(feet<=400){
+    return "This one is on me!"
+  }
+else if(feet>2000 && feet<=2500){
+  return "I will gladly take your thirty bucks."
 }
-
-
-
-  
-
-
+else{
+  return "No can do."
+}
+}
+let city;
 function ternaryCheckCity(city){
-prompt("input city !")
- if (city==="NYC") return( "Ok, sounds good.")
-else {return "No go."}
-
-
-  
-
+  return city==="NYC"? 'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(tips){
-if(tips==="generous") return "Thank you so much.";
-else if (tips==="not as generous"){return "Thank you."}
-else {return "Bye."}
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+    return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+      default:
+        return "Bye."
+}
 }
